418e78f658278b476b2ced79e2d87a70654a0cf4
canavan-a <aidan.canavan3@gmail.com>
2023-12-25 15:17:28 -0500

    fully working

--- app.py
+++ app.py
@@ -1,55 +1,59 @@
-from flask import Flask, redirect, url_for, session, request, jsonify
+from flask import Flask, redirect, url_for, session, request, jsonify, make_response
 import requests
 from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 from dotenv import load_dotenv
+import uuid
 import os
 
 app = Flask(__name__)
 
 load_dotenv()
 
-app.config['GLOBAL_MAP'] = {}
+app.config['SESSION_MAP'] = {}
 
 def set_value(key, value):
     # Set a value in the global map stored in app.config
-    app.config['GLOBAL_MAP'][key] = value
+    app.config['SESSION_MAP'][key] = value
 
 def get_value(key):
     # Get a value from the global map stored in app.config
-    return app.config['GLOBAL_MAP'].get(key)
+    return app.config['SESSION_MAP'].get(key)
 
 GOOGLE_CLIENT_ID = os.environ.get('CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 GOOGLE_REDIRECT_URI = "http://localhost:5000/red"
+LANDING_URL = "https://picstone-generative-ai.vercel.app/"
 
-@app.route('/')
+@app.route('/get_google_oauth_link')
 def root_route():
     
     # get cookie and see if there is an active session
-    # if not do google auth flow
+    session_token = request.cookies.get('session_token')
     
-    authorization_url = f'''https://accounts.google.com/o/oauth2/auth?client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&response_type=code&scope=openid+profile+email&state=your_random_state&prompt=consent'''
-    # authorization_url, _ = flow.authorization_url(prompt='consent')
-    return redirect(authorization_url)
+    if not session_token:
+    
+        authorization_url = f'''https://accounts.google.com/o/oauth2/auth?client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&response_type=code&scope=openid+profile+email&prompt=consent'''
+        return authorization_url
+    else:
+        map_value = get_value(session_token)
+        if not map_value:
+            authorization_url = f'''https://accounts.google.com/o/oauth2/auth?client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&response_type=code&scope=openid+profile+email&prompt=consent'''
+            return authorization_url
+        print(map_value)
+        return f"user already has active session {map_value}"
 
 @app.route('/red')
 def redirect_route():
-    state = request.args.get('state')
     code = request.args.get('code')
-    print(state)
-    print(code)
 
     token_endpoint = 'https://oauth2.googleapis.com/token'
-    client_id = GOOGLE_CLIENT_ID
-    client_secret = GOOGLE_CLIENT_SECRET
-    redirect_uri = 'http://localhost:5000/red'
 
     token_payload = {
         'code': code,
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'redirect_uri': redirect_uri,
+        'client_id': GOOGLE_CLIENT_ID,
+        'client_secret': GOOGLE_CLIENT_SECRET,
+        'redirect_uri': GOOGLE_REDIRECT_URI,
         'grant_type': 'authorization_code'
     }
 
@@ -66,15 +70,39 @@ def redirect_route():
         user_info_response = requests.get(user_info_endpoint, headers=headers)
         user_info = user_info_response.json()
 
-        # Print or use user information as needed
-        print('User Info:', user_info)
+        session_token = str(uuid.uuid4())
+        
+        response = make_response(f'''
+                            <!DOCTYPE html>
+                            <html lang="en">
+                            <head>
+                                <meta charset="UTF-8">
+                                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <title>Redirect Example</title>
+                            </head>
+                            <body>
 
+                            <script>
+                                // JavaScript code to redirect to another page
+                                window.location.href = '{LANDING_URL}';  // Replace with your desired URL
+                            </script>
 
-        return jsonify(user_info)
+                            </body>
+                            </html>
+                            ''')
+
+        response.set_cookie('session_token',
+                                session_token,
+                                httponly=True,
+                                max_age=1000)
+        
+        # add session to global list
+        set_value(session_token, str(user_info['email']))
+
+        return response
     else:
         return jsonify({'error': 'Failed to obtain access token'})
-
-    return 'you have been redirected'
 
 
 if __name__ == '__main__':
